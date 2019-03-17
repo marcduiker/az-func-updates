32e241868b6186303f1a48db9486eb9150f27707
Marc Duiker <marcduiker@gmail.com>
2019-03-17 13:27:09 +0100

    Added feature toggle DoPostUpdate

---
+++ src/AzureFunctionsUpdates.UnitTests/Models/TogglesTests.cs
@@ -0,0 +1,62 @@
+﻿using AzureFunctionsUpdates.Models;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AzureFunctionsUpdates.UnitTests.Models
+{
+    public class TogglesTests
+    {
+        [Fact]
+        public void GivenToggleDoPostUpdateEnvironmentVariableIsSetToNull_WhenRetrievingDoPostUpdate_ThenResultShouldBeFalse()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable(Toggles.DoPostUpdateVariableName, null);
+
+            // Act
+            var result = Toggles.DoPostUpdate;
+
+            // Assert
+            result.Should().BeFalse($"because the {Toggles.DoPostUpdateVariableName} environment variable variable is not set.");
+        }
+
+        [Fact]
+        public void GivenToggleDoPostUpdateEnvironmentVariableIsSetToIncorrectValue_WhenRetrievingDoPostUpdate_ThenResultShouldBeFalse()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable(Toggles.DoPostUpdateVariableName, "notabooleanvalue");
+
+            // Act
+            var result = Toggles.DoPostUpdate;
+
+            // Assert
+            result.Should().BeFalse($"because the {Toggles.DoPostUpdateVariableName} environment variable is not a valid boolean value.");
+        }
+
+        [Fact]
+        public void GivenToggleDoPostUpdateEnvironmentVariableIsSetToFalse_WhenRetrievingDoPostUpdate_ThenResultShouldBeFalse()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable(Toggles.DoPostUpdateVariableName, "false");
+
+            // Act
+            var result = Toggles.DoPostUpdate;
+
+            // Assert
+            result.Should().BeFalse($"because the {Toggles.DoPostUpdateVariableName} environment variable is not a valid boolean value.");
+        }
+
+        [Fact]
+        public void GivenToggleDoPostUpdateEnvironmentVariableIsSetToTrue_WhenRetrievingDoPostUpdate_ThenResultShouldBeTrue()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable(Toggles.DoPostUpdateVariableName, "true");
+
+            // Act
+            var result = Toggles.DoPostUpdate;
+
+            // Assert
+            result.Should().BeTrue($"because the {Toggles.DoPostUpdateVariableName} environment variable is set to true value.");
+        }
+    }
+}
---
+++ src/AzureFunctionsUpdates/Models/Toggles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AzureFunctionsUpdates.Models
+{
+    public class Toggles
+    {
+        public const string DoPostUpdateVariableName = "Toggle_DoPostUpdate";
+
+        public static bool DoPostUpdate
+        {
+            get
+            {
+                var envDoPostupdate = Environment.GetEnvironmentVariable(DoPostUpdateVariableName);
+                if (!string.IsNullOrEmpty(envDoPostupdate) && bool.TryParse(envDoPostupdate, out bool result))
+                {
+                    return result;
+                }
+
+                return false;
+            }
+        }
+    }
+}
